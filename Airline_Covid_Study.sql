aa79c0ae31fda2bf713f7242482dd43d213ef9e2
Heath Herrington <heathherrington@comcast.net>
2020-12-10 18:44:40 -0600

    add sql file

---
+++ Airline_Covid_Study.sql
@@ -0,0 +1,15 @@
+create table airline_covid (
+date date,
+cancelled_rate_AA int,
+total_delay_AA int,
+cancelled_rate_DL int,
+total_delay_DL int,
+cancelled_rate_WN int,
+total_delay_WN int,
+cancelled_rate_UA int,
+total_delay_UA int,
+cancelled_rate_NK int,
+total_delay_NK int,
+cancelled_rate_F9 int,
+total_delay_F9 int,
+cases int)
